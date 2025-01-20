e140c97ffcecace13be7f9e49b0ac043a419b988
Samuel Davis <samueljakdavis@gmail.com>
2025-01-20 12:32:40 -0500

    initialize rot.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Display, Map, RNG } from "rot-js";
+
 export default function App() {
-  return <main />;
+  RNG.setSeed(Math.random());
+  const display = new Display();
+  const { width, height } = display.getOptions();
+  const digger = new Map.Digger(width, height);
+  digger.create(display.DEBUG);
+  for (const room of digger.getRooms())
+    room.getDoors((x, y) => display.draw(x, y, "", "", "red"));
+
+  return (
+    <main>
+      <article>{display.getContainer()}</article>
+    </main>
+  );
 }
